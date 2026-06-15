71679e0e5566d9cbb650f1c977c80e3588fbb054
vsvanshi2207 <vsvanshi2207@gmail.com>
2026-06-15 17:13:16 +0000

    Update function definition

--- 03-callBindApplyFunction/script.js
+++ 03-callBindApplyFunction/script.js
@@ -1,12 +1,13 @@
-function abc(){
-    return this;
-}
 
 document.getElementById("demo").innerHTML = abc();
 
 const person = {
     firstName: "Vanshika",
     age: 25
+}
+
+function abc(){
+    return this.name;
 }
 
 // example of call method
